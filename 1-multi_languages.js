8b40d832e2303753e7246522a8ba538c95383a75
stillwaters <colbertntunyu@gmail.com>
2025-05-25 22:16:47 +0100

    multi_language  file added

---
+++ 1-multi_languages.js
@@ -0,0 +1,8 @@
+const a = 'C is fun';
+const b = 'Python is cool';
+const c = 'JavaScript is amazing'
+
+//console.log(`${a} ${b} ${c}`); // C is fun Python is cool JavaScript is amazing
+console.log(a);
+console.log(b);
+console.log(c)
