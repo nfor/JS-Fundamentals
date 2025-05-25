64bf964a80a7af23ad65f30a2f1e3a53466ed7fb
stillwaters <colbertntunyu@gmail.com>
2025-05-25 23:40:44 +0100

    update to file 3

--- 2-arguments.js
+++ 2-arguments.js
@@ -5,5 +5,5 @@ if (args.length === 0) {
 } else if (args.length === 1) {
   console.log('Argument found');
 } else {
-  console.log(`${args.length} arguments`);
+  console.log(`${args.length} Arguments found`);
 }
